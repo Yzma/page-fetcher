6a1b556c36cac9db1dbbd82643b5217a47be653e
Yzma <10090913+Yzma@users.noreply.github.com>
2023-01-18 10:42:39 -0500

    Remove debug console logs

--- fetcher.js
+++ fetcher.js
@@ -8,9 +8,6 @@ if (args.length <= 1) {
 }
 
 request(args[0], (error, response, body) => {
-  console.log('error:', error) // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
-  console.log('body:', body) // Print the HTML for the Google homepage.
   if (error) {
     console.log('Error: ', error)
     return
